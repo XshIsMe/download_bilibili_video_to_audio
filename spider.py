39e1926b8390a9793800167e7ef95fc44e1dea34
hao <hao@hao.com>
2020-09-21 22:01:33 +0800

    修复多p下载逻辑错误

--- spider.py
+++ spider.py
@@ -12,11 +12,7 @@ def get_video(video_url, video_path):
         file.write(response.content)
 
 
-def get_video_info(bv):
-    # 存储视频信息
-    info_list = []
-    # 生成页面URL
-    page_url = URL_DICT["video_info"].format(bv=bv)
+def get_video_p_info(page_url):
     # 获取页面内容
     response = requests.get(url=page_url, headers=HEADERS_DICT["video_page"])
     # 找到视频标题
@@ -27,13 +23,28 @@ def get_video_info(bv):
     video_url = URL_DICT["video"].format(
         url=re.findall(PATTERN_DICT["video_url"], response.text)[0]
     )
+    return response, title, video_url
+
+
+def get_video_info(bv):
+    # 存储视频信息
+    info_list = []
+    # 生成页面URL
+    page_url = URL_DICT["video_info"].format(bv=bv)
+    # 获取页面内容
+    response, title, video_url = get_video_p_info(page_url)
     # 获取视频p数
     p_count = len(re.findall(PATTERN_DICT["video_p"], response.text))
     # 添加第一p
     info_list.append((title, video_url))
     # 添加后面的p
     for i in range(2, p_count + 1):
-        info_list.append((title + "_p" + str(i), video_url + "?p=" + str(i)))
+        # 生成页面URL
+        page_url = URL_DICT["video_info"].format(bv=bv) + "?p=" + str(i)
+        # 获取页面内容
+        response, title, video_url = get_video_p_info(page_url)
+        # 添加到列表
+        info_list.append((title + "_p" + str(i), video_url))
     return info_list
 
 
